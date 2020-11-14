65afa75dab70297e0c3b99353d285ae43e71dd9b
AngelAlvAlumnCESCristoRey <angelalv.alumn@cescristorey.com>
2020-11-14 12:43:13 +0100

    No consigo que funcione

---
+++ AccesoDatosMySQL/AccesoMysql.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace AccesoDatosMySQL
+{
+    partial class AccesoMysql
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Apellidos = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Telefono = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Direccion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.poblacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataSet1 = new System.Data.DataSet();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Nombre,
+            this.Apellidos,
+            this.Telefono,
+            this.Direccion,
+            this.cp,
+            this.poblacion});
+            this.dataGridView.Location = new System.Drawing.Point(85, 59);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(640, 150);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            // 
+            // Apellidos
+            // 
+            this.Apellidos.HeaderText = "Apellidos";
+            this.Apellidos.Name = "Apellidos";
+            // 
+            // Telefono
+            // 
+            this.Telefono.HeaderText = "Teléfono";
+            this.Telefono.Name = "Telefono";
+            // 
+            // Direccion
+            // 
+            this.Direccion.HeaderText = "Dirección";
+            this.Direccion.Name = "Direccion";
+            // 
+            // cp
+            // 
+            this.cp.HeaderText = "Código Postal";
+            this.cp.Name = "cp";
+            // 
+            // poblacion
+            // 
+            this.poblacion.HeaderText = "Población";
+            this.poblacion.Name = "poblacion";
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // AccesoMysql
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "AccesoMysql";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Apellidos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Telefono;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Direccion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cp;
+        private System.Windows.Forms.DataGridViewTextBoxColumn poblacion;
+        private System.Data.DataSet dataSet1;
+    }
+}
+
---
+++ AccesoDatosMySQL/AccesoMysql.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace AccesoDatosMySQL
+{
+    public partial class AccesoMysql : Form
+    {
+        static MySqlConnection Conex = new MySqlConnection();
+        static string serv = "Server=localhost;";
+        static string db = "Database=agenda;";
+        static string usuario = "UID=root;";
+        static string pwd = "Furipapa86jo";
+        string CadenaDeConexion = (serv + "port=3306;" + usuario + pwd + db + "pooling=false;" +
+            "Allow Zero Datetime=False;Convert Zero Datetime=True");
+        static DataTable tabla;
+        static MySqlDataAdapter dataAdapter = new MySqlDataAdapter("SELECT * FROM contactos",Conex);
+        static MySqlCommandBuilder comandoSQL = new MySqlCommandBuilder(dataAdapter);
+        public void Conectar()
+        {
+            try
+            {
+                Conex.ConnectionString = CadenaDeConexion;
+                Conex.Open();
+                MessageBox.Show("La BD está ahora conectada");
+                
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Ocurrió un error al conectar la BD");
+                
+            }
+        }
+        public static void Desconectar()
+        {
+            Conex.Close();
+        }
+        private void obtenerBaseDatosMySQL()
+        {
+            MySqlDataReader registrosObtenidos = null;
+
+            MySqlCommand cmd = new MySqlCommand("SELECT id_contacto FROM contactos", Conex);
+            try
+            {
+                registrosObtenidos = cmd.ExecuteReader();
+                while (registrosObtenidos.Read())
+                {
+                    dataGridView.Rows.Add(1);
+                    for (int j = 0; j < 6; j++)
+                    {
+                          
+
+                    }
+                } 
+            }
+            catch(Exception e)
+            {
+
+
+            }
+        }
+        public AccesoMysql()
+        {
+            Conectar();
+            InitializeComponent();
+            dataAdapter.Fill(tabla);
+            dataGridView.DataSource = tabla;
+            
+        }
+    }
+}
